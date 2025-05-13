649fb0c6ef60fecacb3cb728c5b0a39e60bae39a
kkkuro90 <114396499+kkkuro90@users.noreply.github.com>
2025-05-13 21:01:11 +0500

    обновил апи, без жёсткой привязки

--- login.js
+++ login.js
@@ -8,7 +8,7 @@ document.querySelector('.register-form').addEventListener('submit', async (e) =>
     const password = document.getElementById('password').value;
 
     try {
-        const response = await fetch('http://localhost:5001/api/auth/register', {
+        const response = await fetch('/api/auth/register', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
